10a195cf34ca6419d51e23805552dcf43a24454f
Nathan Kau <nathankau@gmail.com>
2020-06-11 00:07:36 -0700

    tuning

--- src/CVXTest.jl
+++ src/CVXTest.jl
@@ -1,10 +1,11 @@
 include("CVX.jl")
 include("Parameters.jl")
+include("ADMM.jl")
 
 ##
 α = 1 # L2 weighting
 ρ = 1
-N = 100 # knot points
+N = 1000 # knot points
 
 params = Problem(α, ρ, N)
 X, U, p = CVX(params)
@@ -13,7 +14,10 @@ Xs = StateTrajectoryToArray(X.value, params)
 Us = ControlTrajectoryToArray(U.value, params)
 U_norm = [norm(Us[:,i]) for i = 1:params.N]
 
+println("\n\n -------------- CVX ------------")
 @show p.optval
+@show objective(X.value, U.value, params)
+
 
 ##
 plot(U_norm)
